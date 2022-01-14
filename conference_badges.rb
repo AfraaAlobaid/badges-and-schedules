a12780ef3c037a6b14c1142f1f828615879419af
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-14 15:37:28 +1030

    Used each and each_with_index for iterations

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+    names.collect do |name|
+        "Hello, my name is #{name}."
+    end 
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    counter = 1
+    attendees.each_with_index do |name, index|
+        room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+        counter += 1
+    end
+    room_assignments
+end 
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |name_message|
+        puts name_message
+    end 
+
+    assign_rooms(attendees).each do |room|
+        puts room
+    end 
+end
+
